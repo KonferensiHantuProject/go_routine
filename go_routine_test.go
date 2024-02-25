0337b8aa810098c2469af03fdb97e2f3038cc51f
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2024-02-25 09:24:12 +0700

    Light Goroutine

--- go_routine_test.go
+++ go_routine_test.go
@@ -16,3 +16,15 @@ func TestCreateGoRoutines(t *testing.T) {
 
 	time.Sleep(1 * time.Second)
 }
+
+func DisplayNumber(number int) {
+	fmt.Println("Nomor ", number)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+
+	time.Sleep(10 * time.Second)
+}
